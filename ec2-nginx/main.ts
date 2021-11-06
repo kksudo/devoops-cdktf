8a58dea8ed0d25b3f3b219a0a215fa796f7e0443
Kirill <kirill.kazakov@simplinic.com>
2021-11-06 15:24:10 +0300

    orig

--- ec2-nginx/main.ts
+++ ec2-nginx/main.ts
@@ -1,50 +1,26 @@
-import {Construct} from "constructs";
-import {App, TerraformOutput, TerraformStack} from "cdktf";
-import {AwsProvider, Instance, SecurityGroup} from './.gen/providers/aws'
+import { Construct } from 'constructs'
+import { App, TerraformStack, TerraformOutput } from 'cdktf'
+import { AwsProvider, Instance } from './.gen/providers/aws'
 
 class MyStack extends TerraformStack {
-    constructor(scope: Construct, id: string) {
-        super(scope, id);
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
 
-        new AwsProvider(this, 'aws', {
-            region: 'eu-central-1',
-        })
+    new AwsProvider(this, 'aws', {
+      region: 'us-west-1',
+    })
 
-        // new SecurityGroup(this, "allow http", {
-        //     name: "allow-http-web",
-        //     description: "Allow all incoming http traffic",
-        //     egress: [
-        //         {
-        //             cidrBlocks: ["0.0.0.0/0"],
-        //             description: "Allow all outgoing traffic",
-        //             fromPort: 0,
-        //             protocol: "-1",
-        //             toPort: 0,
-        //         },
-        //     ],
-        //     ingress: [
-        //         {
-        //             cidrBlocks: "0.0.0.0/0",
-        //             description: "Allow all incoming http traffic",
-        //             fromPort: 0,
-        //             protocol: -1,
-        //             self: true,
-        //             toPort: 0,
-        //         },
-        //     ],
-        // });
+    const instance = new Instance(this, 'compute', {
+      ami: 'ami-01456a894f71116f2',
+      instanceType: 't2.micro',
+    })
 
-        const instance = new Instance(this, 'compute', {
-            ami: 'ami-01456a894f71116f2',
-            instanceType: 't2.micro',
-        })
-
-        new TerraformOutput(this, 'public_ip', {
-            value: instance.publicIp,
-        })
-    }
+    new TerraformOutput(this, 'public_ip', {
+      value: instance.publicIp,
+    })
+  }
 }
 
-const app = new App();
-new MyStack(app, "typescript-aws");
-app.synth();
+const app = new App()
+new MyStack(app, 'typescript-aws')
+app.synth()
