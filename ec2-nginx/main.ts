497dfce8bec6b6376d593c15219014ac279a9374
Kirill <kirill.kazakov@simplinic.com>
2021-11-06 15:51:24 +0300

    Default example working

--- ec2-nginx/main.ts
+++ ec2-nginx/main.ts
@@ -1,26 +1,29 @@
-import { Construct } from 'constructs'
-import { App, TerraformStack, TerraformOutput } from 'cdktf'
-import { AwsProvider, Instance } from './.gen/providers/aws'
+import { Construct } from "constructs";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { AwsProvider, EC2 } from "./.gen/providers/aws";
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id)
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
 
+    // Configures the AWS provider
     new AwsProvider(this, 'aws', {
       region: 'us-west-1',
     })
 
-    const instance = new Instance(this, 'compute', {
+    // Configures the EC2 instance
+    const instance = new EC2.Instance(this, 'compute', {
       ami: 'ami-01456a894f71116f2',
       instanceType: 't2.micro',
     })
 
+    // Output similar terraform output
     new TerraformOutput(this, 'public_ip', {
       value: instance.publicIp,
     })
   }
 }
 
-const app = new App()
-new MyStack(app, 'typescript-aws')
-app.synth()
+const app = new App();
+new MyStack(app, "ec2-nginx");
+app.synth();
